d61f8455744ae79444d4f3b843731f7b95b3221b
coderahmadi <s.ahmadi.musavi95@gmail.com>
2024-05-08 00:19:15 +0200

    a change

--- assets/js/jsFile.js
+++ assets/js/jsFile.js
@@ -1,14 +1,14 @@
 const data_ltr = {
-  headerTitle: "Personal Info",
+  headerTitle: "Persönliche Informationen",
   myFullName: "Sayed Jamal Hussain Ahmadi",
   myRole: "Full-Stack-Softwareentwickler",
   location: "Berlin, Deutschland",
   sumTitle: "Professionelle Zusammenfassung:",
   sumContent:
     "Mehr als 4 Jahre Erfahrung in der Entwicklung von Full-Stack-Anwendungen. Nachgewiesene Fähigkeit zur Projektleitung während meines letzten Projekts, BioBaumBauer. Erfahrung in der API-Entwicklung und der Integration von Bibliotheken und Tools von Drittanbietern. Fundierte Kompetenz im SDLC, insbesondere in agilen Techniken. Etablierte Kapazität in CI/CD-Pipeline und DevOps-Projekten zur Rationalisierung der Entwicklung. Entwicklung einer reaktionsfähigen und ausgefeilten Benutzeroberfläche (Client), die mit REST-APIs interagiert.",
-  eduTitle: "Weiterbildung",
-  eduSummary:
-    "Die Menschen am Digital Career Institute eint eine Mission: Zunächst unterstützten wir Flüchtlinge bei der Integration in den digitalen Arbeitsmarkt.",
+  expTitle: "Aktuelle Berufserfahrung",
+  expSummary:
+    "Führte wöchentlich mehr als 3 Stunden Nachhilfeunterricht für mehr als 10 unserer Klassenkameraden durch und steigerte deren Entwicklungskompetenz durch die Ausübung der Gedankenthemen und die Beantwortung komplexer Fragen durch Live-Codierung.",
 };
 const data_rtl = {
   headerTitle: "معلومات شخصی",
@@ -18,14 +18,13 @@ const data_rtl = {
   sumTitle: " خلاصه حرفه ای:",
   sumContent:
     "بیش از 4 سال تجربه در توسعه برنامه های فول استک. در آخرین پروژه من، BioBaumBauer، توانایی در رهبری پروژه را نشان داد. تجربه در توسعه API و یکپارچه سازی کتابخانه ها و ابزارهای شخص ثالث. شایستگی عمیق در SDLC به ویژه در تکنیک های چابک. ظرفیت ایجاد شده در پروژه‌های خط لوله CI/CD و DevOps برای ساده‌سازی توسعه. UI (مشتری) پاسخگو و پیچیده در تعامل با REST API.",
-  eduTitle: "ادامه تحصیل:",
-  eduSummary:
-    "افراد مؤسسه شغلی دیجیتال با یک مأموریت متحد شده اند: در ابتدا، ما از پناهندگان حمایت کردیم تا در بازار کار دیجیتال ادغام شوند.",
+  expTitle: "تجربه کاری:",
+  expSummary:
+    "هفته ای 3 ساعت جلسه آموزشی برای بیش از 10 نفر از همکلاسی هایمان برگزار کردیم و با تمرین موضوعات فکری و پاسخ به سؤالات پیچیده از طریق کدنویسی زنده، صلاحیت توسعه آنها را افزایش دادیم.",
 };
 
 document.getElementById("language").addEventListener("change", function () {
   const directionValue = this.value;
-  console.log("Value: ", directionValue);
   const outerMostTag = document.getElementById("outerMost");
   outerMostTag.setAttribute("dir", directionValue);
 
@@ -35,8 +34,8 @@ document.getElementById("language").addEventListener("change", function () {
   const location = document.querySelector("#location");
   const summaryTitle = document.querySelector("#summaryTitle");
   const summaryText = document.querySelector("#summaryText");
-  const eduTitle = document.querySelector("#eduTitle")
-  const eduSummary = document.querySelector("#eduSummary")
+  const expTitle = document.querySelector("#expTitle")
+  const expSummary = document.querySelector("#expSummary")
 
   if (directionValue === "ltr") {
     headTopP.innerText = data_ltr.headerTitle;
@@ -45,8 +44,8 @@ document.getElementById("language").addEventListener("change", function () {
     location.innerText = data_ltr.location;
     summaryTitle.innerText = data_ltr.sumTitle;
     summaryText.innerText = data_ltr.sumContent;
-    eduTitle.innerText = data_ltr.eduTitle;
-    eduSummary.innerText = data_ltr.eduSummary;
+    expTitle.innerText = data_ltr.expTitle;
+    expSummary.innerText = data_ltr.expSummary;
   }
   if (directionValue === "rtl") {
     headTopP.innerText = data_rtl.headerTitle;
@@ -55,7 +54,7 @@ document.getElementById("language").addEventListener("change", function () {
     location.innerText = data_rtl.location;
     summaryTitle.innerText = data_rtl.sumTitle;
     summaryText.innerText = data_rtl.sumContent;
-    eduTitle.innerText = data_rtl.eduTitle;
-    eduSummary.innerText = data_rtl.eduSummary;
+    expTitle.innerText = data_rtl.expTitle;
+    expSummary.innerText = data_rtl.expSummary;
   }
 });
